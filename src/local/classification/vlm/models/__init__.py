6cf73447cf9c47ecf9ba31c5c9175de5462d3f17
Mason Jeffrey <mason.ezra.projects@gmail.com>
2026-05-27 23:12:28 +1000

    patch

---
+++ src/local/classification/vlm/models/__init__.py
@@ -0,0 +1,3 @@
+from .openai_clip import load_openai_clip, make_openai_clip_loader
+from .biomedclip import load_biomedclip, make_biomedclip_loader
+from .unimedclip import load_unimedclip, make_unimedclip_loader
